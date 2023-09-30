0be34e7d31b63290acd9b7bdfffe76320e62c5ae
mlwelker <nutria.watts.0g@icloud.com>
2023-09-29 23:23:16 -0500

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Michael Welker on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
