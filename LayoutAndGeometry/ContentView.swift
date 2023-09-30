d51f85e56507b888c83c4252a283ba9c97ff5afd
mlwelker <nutria.watts.0g@icloud.com>
2023-09-30 16:46:09 -0500

    text in frame with alignment and offset

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -3,13 +3,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Live long and prosper.")
+            .frame(minWidth: 300, minHeight: 300, alignment: .topLeading)
+            .offset(x: 20, y: 20)
     }
 }
 
